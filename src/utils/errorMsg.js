a66d2627521812c4cdf55acd2eee46a9d65e28c8
gitmvfs <marcossantos8002@gmail.com>
2024-03-21 18:21:41 -0300

    refactor(tratarMensagemDeErro): agora os erros não tratados são exibidos diferentes

--- src/utils/errorMsg.js
+++ src/utils/errorMsg.js
@@ -1,12 +1,14 @@
 function tratarMensagensDeErro(err) {
 
+    try {
+        let errMsg = err.errors[0].message
 
-    let errMsg = err.errors[0].message
+        if (errMsg.includes("PRIMARY must be unique")) errMsg = "Dado já cadastrado, consulte os relatórios ou contate o administrador.";
 
-
-    if(errMsg.includes("PRIMARY must be unique")) errMsg = "Dado já cadastrado, consulte os relatórios ou contate o administrador.";
-
-    return errMsg
+        return errMsg
+    }
+    catch{
+        return "Mensagem de erro não tratada :" + err
+    }
 }
-
 module.exports = tratarMensagensDeErro
