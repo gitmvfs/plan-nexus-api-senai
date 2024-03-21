1fd479fb83f0cd8d2e83610239d26c30d44e95f6
gitmvfs <marcossantos8002@gmail.com>
2024-03-21 04:49:41 -0300

    feat(statusArmario): adicionado modelagem sequelize do statusArmario

---
+++ src/models/statusArmario.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const conectar_db = require("../services/conectarDB")
+const sequelize = conectar_db
+
+const statusArmarioModel = sequelize.define('status', {
+    id_status: {
+        type: DataTypes.TINYINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    status: {
+        type: DataTypes.ENUM('Ocupado', 'Desocupado', 'Trancado'),
+        defaultValue: 'Desocupado' // Defina o valor padrão se necessário
+    }
+}, {
+    tableName: 'status',
+    timestamps: false // Se não houver colunas de timestamp
+});
+
+statusArmarioModel.sync()
+
+module.exports = statusArmarioModel
