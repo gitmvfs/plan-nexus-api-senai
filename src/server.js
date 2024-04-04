8e19346913befd0488a3fa14d159603a8df52ea9
gitmvfs <marcossantos8002@gmail.com>
2024-04-04 14:43:41 -0300

    feat(server): adicionado funcionarios ao servidor

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const cors = require("cors")
 //Importando as rotas
 const alunosRota = require("./routes/alunoRota")
 const armarioRota = require("./routes/armarioRota")
-
+const funcionarioRota = require("./routes/funcionarioRota")
 
 // Config dotenv
 dotenv.config({ path: resolve("../", ".env") })
@@ -23,5 +23,6 @@ app.use(cors())
 //Configurando rotas
 app.use("/aluno", alunosRota)
 app.use("/armario", armarioRota)
+app.use("/funcionario", funcionarioRota)
 
 app.listen(serverPort, () => console.log(`HTTP RUNNING AT: http://localhost:${serverPort}`))
