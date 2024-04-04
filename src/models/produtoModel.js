2305686c4eb5d74a679fbf3d2923da2b1a1de7ee
gitmvfs <marcossantos8002@gmail.com>
2024-04-04 17:05:17 -0300

    feat(produtoModel): Feito modelo de produto sequelize

---
+++ src/models/produtoModel.js
@@ -0,0 +1,45 @@
+const conectarDB = require("../services/conectarDB")
+const { DataTypes } = require("sequelize")
+const sequelize = conectarDB
+
+const produtoModel = sequelize.define('produto', {
+    id_produto: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nome: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    descricao: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    foto: {
+      type: DataTypes.JSON,
+      allowNull: false
+    },
+    qtd_estoque: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    valor: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: false
+    },
+    brinde: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: true
+    }
+  }, {
+    tableName: 'produto',
+    timestamps: false // Se não tiver campos de created_at e updated_at
+  });
+  
+module.exports = produtoModel
