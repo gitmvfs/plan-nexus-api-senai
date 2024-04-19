c65de8b76c90dfe2455b819a8df23e4eda473167
gitmvfs <marcossantos8002@gmail.com>
2024-04-18 22:18:15 -0300

    feat: modulo visualizar todas doacoes armario

--- src/controllers/doacaoArmarioController.js
+++ src/controllers/doacaoArmarioController.js
@@ -3,10 +3,10 @@ function cadastroDoacaoArmario(doacaoArmario, sequelize) {
     return new Promise((resolve, reject) => {
 
         try {
-            const {numeroArmario, idAluno, data } = doacaoArmario
+            const { numeroArmario, idAluno, data } = doacaoArmario
 
             sequelize.query("call doar_armario(?,?,?)", {
-                replacements: [  idAluno,numeroArmario, data],
+                replacements: [idAluno, numeroArmario, data],
                 type: sequelize.QueryTypes.INSERT
             })
                 .then(r => resolve(r))
@@ -20,4 +20,20 @@ function cadastroDoacaoArmario(doacaoArmario, sequelize) {
     })
 }
 
-module.exports = {cadastroDoacaoArmario}
+function visualizarTodasDoacoesArmario(sequelize) {
+
+    return new Promise((resolve, reject) => {
+        try {
+            sequelize.query("select * from doacao_armario order by data;", {
+                type: sequelize.QueryTypes.SELECT
+            })
+                .then(r => resolve(r))
+                .catch(e => reject(e));
+        }
+        catch (err) {
+            reject(err)
+        }
+    })
+}
+
+module.exports = { cadastroDoacaoArmario, visualizarTodasDoacoesArmario }
--- src/routes/doacaoArmarioRota.js
+++ src/routes/doacaoArmarioRota.js
@@ -1,6 +1,6 @@
 const { tratarMensagensDeErro } = require("../utils/errorMsg")
 const authMiddleware = require("../middleware/auth")
-const { cadastroDoacaoArmario } = require("../controllers/doacaoArmarioController")
+const { cadastroDoacaoArmario, visualizarTodasDoacoesArmario } = require("../controllers/doacaoArmarioController")
 
 const router = require("express").Router()
 
@@ -9,17 +9,35 @@ router.use(authMiddleware)
 router.post("/cadastro", async (req, res) => {
 
     try {
-        const { numeroArmario, idAluno} = req.body
+        const { numeroArmario, idAluno } = req.body
         const data = new Date(req.body.data)
         const doacaoArmario = { numeroArmario, idAluno, data }
 
         await cadastroDoacaoArmario(doacaoArmario, req.sequelize)
         res.status(201).json({ "msg": "Doação de armario criado com sucesso", "statusCode": "201" })
     }
-    catch(err){
+    catch (err) {
         const erroTratado = await tratarMensagensDeErro(err)
         res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
     }
 })
+
+router.get("/todos", async (req, res) => {
+
+
+    try {
+        const response = await visualizarTodasDoacoesArmario(req.sequelize)
+        res.status(200).json({ "msg": "Doação de armario criado com sucesso", "statusCode": "200", "response":response })
+
+    }
+    catch (err) {
+        const erroTratado = await tratarMensagensDeErro(err)
+        res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
+    }
+
+
+
+})
+
 
 module.exports = router
