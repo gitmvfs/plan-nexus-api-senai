acad7e92ff83f5c2c04c439824c08f4084409184
LaizaBarbosa <laiza0700@gmail.com>
2024-04-21 21:48:48 -0300

    fix: consertado bug que retornava status 200 independente da resposta

--- src/routes/reservaRota.js
+++ src/routes/reservaRota.js
@@ -3,7 +3,6 @@ const authMiddleware = require("../middleware/auth")
 const reservaModel = require("../models/reservaModel")
 const {tratarMensagensDeErro} = require("../utils/errorMsg")
 const {criarReserva, pesquisarUmaReserva, cancelarReserva, visualizarTodasReservas} = require("./../controllers/reservaController")
-const dataLocal = require("../utils/data")
 const sequelize = require("sequelize")
 
 router.use(authMiddleware)
@@ -13,17 +12,15 @@ router.post("/criar", async(req, res) => {
     const {fk_aluno, fk_produto, quantidade} = req.body
     const dataRetirada = new Date(req.body.retirada)
     const reserva = {fk_aluno, fk_produto, quantidade, dataRetirada}
-    try {
 
+    try {
         const response = await criarReserva(reserva, req.sequelize)
         res.status(201).json({ "msg": `reserva criada: ${reserva}`, "statusCode": 201, "response": response })
-
-
     } catch (err) {
         const erroTratado = await tratarMensagensDeErro(err)
         res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
     }
-    // console.log(reserva)
+
 })
 
 router.get("/todas", async (req, res) =>{
@@ -36,7 +33,6 @@ router.get("/todas", async (req, res) =>{
     } catch (err) {
         const erroTratado = await tratarMensagensDeErro(err)
         res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
-    
     }
 })
 
@@ -44,11 +40,10 @@ router.get("/:id_reserva", async (req, res) =>{
     const {id_reserva} = req.params
     
     try {
-        const response = await pesquisarUmaReserva(id_reserva, req.sequelize)
-        .then((response) => {
-            res.status(200).json({ msg: "reserva encontrada", "statusCode": 200, "response":response });
-        })
-        .catch((e) => res.status(400).json({ msg: "Erro ao realizar consulta", "statusCode": 400, errMsg: e }));
+        const reserva = await pesquisarUmaReserva(id_reserva, req.sequelize)
+        reserva.length === 0
+        ? res.status(404).json({ msg: "reserva não encontrada", "statusCode": 404, "response": reserva })
+        : res.status(200).json({ msg: "reserva encontrada", "statusCode": 200, "response": reserva })
         
     } catch (err) {
         const erroTratado = await tratarMensagensDeErro(err)
@@ -62,13 +57,14 @@ router.get("/:id_reserva", async (req, res) =>{
 router.patch("/:id_reserva", async (req, res) => {
     const {id_reserva} = req.params
 
+
     try {
         const response = await cancelarReserva(id_reserva, req.sequelize)
-        .then((response) => {
-            res.status(200).json({ msg: "reserva cancelada com sucesso", "statusCode": 200, "response": response });
-        })
-        .catch((e) => console.log(e));
-    } catch (error) {
+        response == false
+        ?  res.status(400).json({ msg: "não é possível cancelar a reserva", "statusCode": 400, "response": response })
+        :  res.status(200).json({ msg: "reserva cancelada com sucesso", "statusCode": 200, "response": response })
+        
+    } catch (err) {
         const erroTratado = await tratarMensagensDeErro(err)
         res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
     }
@@ -81,7 +77,7 @@ router.patch("/:id_reserva", async (req, res) => {
     try {
         const response = await confirmarReserva(id_reserva, req.sequelize)
         .then((response) => {
-            res.status(200).json({ msg: "reserva confirmada com sucesso", "statusCode": 200, "response": response });
+            res.status(200).json({ msg: "reserva entregue", "statusCode": 200, "response": response });
         })
         .catch((e) => console.log(e));
 
