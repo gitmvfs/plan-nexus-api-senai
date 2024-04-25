4f0ba360391160d3ed383be3be2c8261285a0579
gitmvfs <marcossantos8002@gmail.com>
2024-04-25 10:38:26 -0300

    refactor: refatorado produto para aceitar fotos
    ainda falta construir o controller para lidar com os fieldsName

--- src/routes/produtoRota.js
+++ src/routes/produtoRota.js
@@ -1,34 +1,46 @@
 const router = require("express").Router()
 const { cadastrarProduto } = require("../controllers/produtoController")
 const authMiddleware = require("../middleware/auth")
-const {produtoValidacao} = require("../utils/validacao")
-const {tratarMensagensDeErro} = require("../utils/errorMsg")
+const { produtoValidacao } = require("../utils/validacao")
+const { tratarMensagensDeErro } = require("../utils/errorMsg")
+const { uploadProduto, uploadImagem } = require("../utils/multer")
+const {salvarImagemAzure, excluirImagemAzure} = require("../controllers/blobController")
 
 router.use(authMiddleware)
 
-router.post("/cadastro", async (req,res) =>{
-    try{
-    const {nome,cores,tamanhos,fotos,valor,descricao,brinde } = req.body
-  
-    const produto = {
-        nome,
-        cores,
-        tamanhos,
-        fotos,
-        valor,
-        descricao,
-        brinde
+router.post("/cadastro",uploadImagem.any() ,async (req, res) => {
+    try {
+        const { nome, descricao } = req.body
+        
+        // define a lista de cores e tamanhos
+        const cores = req.body.cores.split(",")
+        const tamanhos = req.body.tamanhos.split(",")
+        const valor =  Number(req.body.valor)
+        const brinde = Boolean(req.body.brinde)
+
+        const imagensAgrupadas = uploadImagem
+        console.log(req.files)
+
+        // console.log(uploadImagem)
+        const produto = {
+            nome,
+            cores,
+            tamanhos,
+            valor,
+            descricao,
+            brinde
+        }
+
+        // console.log(produto)
+        const produtoValidado = produtoValidacao.parse(produto)
+        // const response = await cadastrarProduto(produtoValidado, req.sequelize)
+        res.json({"statusCode": 201, "msg": "Produto cadastrado com sucesso" })
     }
+    catch (err) {
+        const erroTratado = await tratarMensagensDeErro(err)
+        res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
 
-    const produtoValidado = produtoValidacao.parse(produto)
-    const response = await cadastrarProduto(produtoValidado,req.sequelize)
-    res.json({...response})
-}
-catch(err){
-    const erroTratado = await tratarMensagensDeErro(err)
-    res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
-
-}
+    }
 })
 
 module.exports = router
