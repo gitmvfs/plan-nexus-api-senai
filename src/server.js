285d50539a5a81b2571255e90a4603a33e53ec44
LaizaBarbosa <laiza0700@gmail.com>
2024-05-04 18:17:52 -0300

    feat: adicionada rota de associado no server.js

--- src/server.js
+++ src/server.js
@@ -16,6 +16,7 @@ const doacaoDinheiroRota = require("./routes/doacaoDinheiroRota")
 const doacaoArmarioRota = require("./routes/doacaoArmarioRota")
 const reservaRota = require("./routes/reservaRota")
 const turmaRota = require("./routes/turmaRota")
+const associadoRota = require("./routes/associadoRota")
 // Config dotenv
 dotenv.config({ path: resolve("../", ".env") })
 const serverPort = process.env.PORT || 3333
@@ -57,5 +58,6 @@ app.use("/doacaoDinheiro", doacaoDinheiroRota)
 app.use("/doacaoArmario", doacaoArmarioRota)
 app.use("/reserva", reservaRota)
 app.use("/turma", turmaRota)
+app.use("/associado", associadoRota)
 
 app.listen(serverPort, () => console.log(`HTTP RUNNING AT: http://localhost:${serverPort}`))
