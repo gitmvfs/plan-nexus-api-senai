2c418f892afcde6f32274624a59b70ffd71d2785
marcossantos8002@gmail.com <marcossantos8002@gmail.com>
2024-06-04 20:57:43 -0300

    feat: calcular carrinho / simular desconto

--- src/controllers/carrinhoComprasController.js
+++ src/controllers/carrinhoComprasController.js
@@ -1,5 +1,6 @@
 const { novoErro } = require("../utils/errorMsg")
-const { pesquisarProdutoAtivoPeloId } = require("./produtoController")
+const { pesquisarUmAssociadoPeloId } = require("./associadoController")
+const { pesquisarProdutoAtivoPeloId, pesquisarProdutoPeloId } = require("./produtoController")
 
 function adicionarItemCarrinho(idProduto, quantidadePedido, aluno, sequelize) {
 
@@ -26,10 +27,11 @@ function adicionarItemCarrinho(idProduto, quantidadePedido, aluno, sequelize) {
             }
 
             // adiciona o produto a lista
+            console.log(idProduto, quantidadePedido)
             listaCarrinho[`${idProduto}`] = quantidadePedido
 
             // manda para o banco
-            await sequelize.query("call adicionar_item_carrinho(? , ?)", {
+            await sequelize.query("call atualizar_carrinho(? , ?)", {
                 replacements: [idCarrinho, JSON.stringify(listaCarrinho)],
                 type: sequelize.QueryTypes.UPDATE
             })
@@ -55,16 +57,16 @@ function removerItemCarrinho(idProduto, aluno, sequelize) {
             if (listaCarrinho.hasOwnProperty(idProduto)) {
                 delete listaCarrinho[`${idProduto}`]
             }
-            else{
+            else {
                 reject(novoErro("Id do Produto não está presente no carrinho", 400))
                 return
             }
             // manda para o banco
-            await sequelize.query("call adicionar_item_carrinho(? , ?)", {
+            await sequelize.query("call atualizar_carrinho(? , ?)", {
                 replacements: [idCarrinho, JSON.stringify(listaCarrinho)],
                 type: sequelize.QueryTypes.UPDATE
             })
-            .catch((err) => reject(err))
+                .catch((err) => reject(err))
 
             resolve(listaCarrinho)
 
@@ -80,11 +82,11 @@ function pesquisarCarrinhoPeloId(idAluno, sequelize) {
     return new Promise(async (resolve, reject) => {
         try {
 
-            await sequelize.query("select * from todos_carrinhos where fk_aluno = ?", {
+            await sequelize.query("select * from todos_carrinhos where id_aluno = ?", {
                 replacements: [idAluno],
                 type: sequelize.QueryTypes.SELECT
             })
-                .then((r) => !!r[0] == false ? resolve({}) : resolve(r[0].itens_carrinho))
+                .then((r) => !!r[0].itens_carrinho == false ? resolve({}) : resolve(r[0].itens_carrinho))
                 .catch((err) => reject(err))
         }
         catch (err) {
@@ -94,8 +96,89 @@ function pesquisarCarrinhoPeloId(idAluno, sequelize) {
 
 }
 
-function valorCarrinhoCompras() {
+function criarReserva(tipo_pagamento, aluno, sequelize) {
+
+    return Promise((resolve, reject) => {
+
+        cons
+
+
+    })
 
 }
 
-module.exports = { adicionarItemCarrinho, removerItemCarrinho, valorCarrinhoCompras }
+function valorCarrinhoCompras(aluno, sequelize) {
+
+    return new Promise(async (resolve, reject) => {
+
+        try {
+            const idCarrinho = aluno.id_aluno
+            const listaCarrinho = await pesquisarCarrinhoPeloId(idCarrinho, sequelize)
+            let alunoSocio = false
+
+            // Define se o aluno é sócio
+            await pesquisarUmAssociadoPeloId(aluno.id_aluno, sequelize)
+                .then(() => resultadoSocio = true)
+                .catch(() => resultadoSocio = false)
+
+            let valorTotal = 0
+
+            if (Object.keys(listaCarrinho).length === 0) {
+                resolve(valorTotal)
+            }
+
+
+            const calcValorProduto = Object.keys(listaCarrinho).map(async (key) => {
+                const produto = await pesquisarProdutoAtivoPeloId(key, sequelize);
+
+                if (alunoSocio) {
+                    valorTotal += (Number(produto.valor) - Number(produto.desconto_associado)) * Number(listaCarrinho[key]);
+                } else {
+                    valorTotal += Number(produto.valor) * Number(listaCarrinho[key]);
+                }
+            });
+
+            await Promise.all(calcValorProduto);
+            resolve(valorTotal)
+        }
+        catch (err) {
+            reject(err)
+        }
+
+
+    })
+
+}
+
+function simularDesconto(aluno, sequelize) {
+    return new Promise(async (resolve, reject) => {
+
+        try {
+            const idCarrinho = aluno.id_aluno
+            const listaCarrinho = await pesquisarCarrinhoPeloId(idCarrinho, sequelize)
+            let valorDesconto = 0
+            let valorTotal = 0
+
+            const calcValorProduto = Object.keys(listaCarrinho).map(async (key) => {
+                const produto = await pesquisarProdutoAtivoPeloId(key, sequelize);
+
+                valorDesconto += (Number(produto.valor) - Number(produto.desconto_associado)) * Number(listaCarrinho[key]);
+                valorTotal += Number(produto.valor) * Number(listaCarrinho[key]);
+            });
+
+            await Promise.all(calcValorProduto);
+            resolve({ "Valor com desconto": valorDesconto, "valor sem desconto": valorTotal })
+        }
+        catch (err) {
+            reject(err)
+        }
+
+
+    })
+}
+
+// function valorAssociadoCarrinhoCompras(idCarrinho, aluno, sequelize){
+
+// }
+
+module.exports = { adicionarItemCarrinho, removerItemCarrinho, valorCarrinhoCompras, valorCarrinhoCompras, simularDesconto }
