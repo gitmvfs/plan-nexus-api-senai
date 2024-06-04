04079614b0c81ed3847b8174bce23c15d4e1b4f4
marcossantos8002@gmail.com <marcossantos8002@gmail.com>
2024-06-04 20:59:10 -0300

    refactor: separado rota ERP do e-commerce

--- src/routes/alunoRota.js
+++ src/routes/alunoRota.js
@@ -1,5 +1,4 @@
 const router = require("express").Router()
-const { response } = require("express")
 const { cadastroMultiplosAlunos, atualizarAluno, cadastroUnicoAluno, pesquisaAluno, pesquisaTodosAlunos, pesquisarAlunoPorCpf, loginAluno } = require("../controllers/alunoController")
 const { cadastroDeTurmas } = require("../controllers/cursoController")
 const { authMiddleware } = require("../middleware/auth_funcionario")
@@ -8,60 +7,6 @@ const excelToJson = require("../utils/excelParseJson")
 const { uploadArquivoAlunos, uploadImagem } = require("../utils/multer")
 const { alunoUnicoValidacao } = require("../utils/validacao")
 const { retirarFormatacao } = require("../utils/converterString")
-const { authMiddleware_aluno } = require("../middleware/auth_aluno")
-const { adicionarItemCarrinho, removerItemCarrinho } = require("../controllers/carrinhoComprasController")
-
-
-router.post("/login", async (req, res) => {
-
-
-    try {
-        const { email, senha } = req.body
-
-        const aluno = { email, senha }
-
-        const response = await loginAluno(aluno)
-        !!response == true
-            ? res.status(200).json({ "statusCode": "200", "msg": "Logado com sucesso", "response": response })
-            : res.status(400).json("Usuario ou senha inválidos")
-    }
-    catch (err) {
-        const erroTratado = await tratarMensagensDeErro(err)
-        res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
-    }
-})
-
-// ROTAS PROTEGIDAS ALUNOS
-
-router.patch("/carrinhoCompras/adicionar", authMiddleware_aluno, async (req, res) => {
-
-    try {
-        const { idProduto, quantidade } = req.body
-
-        const response = await adicionarItemCarrinho(idProduto, quantidade, req.aluno, req.sequelize)
-        res.status(200).json({ "msg": "Carrinho atualizado com sucesso", "statusCode": 201, "response": response })
-    }
-    catch (err) {
-        const erroTratado = await tratarMensagensDeErro(err)
-        res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
-    }
-
-})
-
-router.patch("/carrinhoCompras/remover", authMiddleware_aluno, async (req, res) => {
-
-    try {
-        const { idProduto } = req.body
-
-        const response = await removerItemCarrinho(idProduto, req.aluno, req.sequelize)
-        res.status(200).json({ "msg": "Carrinho atualizado com sucesso", "statusCode": 201, "response": response })
-    }
-    catch (err) {
-        const erroTratado = await tratarMensagensDeErro(err)
-        res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
-    }
-
-})
 
 
 // ROTAS PROTEGIDAS FUNCIONARIOS
