70d4ad5e463cd8c9c024bd74fe41e43630d9b166
marcossantos8002@gmail.com <marcossantos8002@gmail.com>
2024-06-11 06:00:44 -0300

    feat: grafico associados comparativo

--- src/controllers/graficosController.js
+++ src/controllers/graficosController.js
@@ -1,3 +1,4 @@
+const { novoErro } = require("../utils/errorMsg")
 const { pesquisarProdutoPeloId } = require("./produtoController")
 
 function gerarGraficos(sequelize) {
@@ -16,7 +17,6 @@ function gerarGraficos(sequelize) {
         }
     })
 }
-
 
 function estoqueInfo(response, sequelize) {
 
@@ -129,7 +129,6 @@ function itensMaisVendidosInfo(response, sequelize) {
     })
 }
 
-
 function agruparPorNomeCor(produtos) {
     const agrupados = {};
 
@@ -175,4 +174,223 @@ function listarProdutosMaisDoados(doacoes) {
     return listaDeDoacoes;
 }
 
-module.exports = { gerarGraficos }
+function gerarGraficosComparativos(sequelize) {
+    return new Promise(async (resolve, reject) => {
+
+        try {
+            let response = {}
+            response = graficoAssociadosComparativo(response, sequelize)
+            resolve(response)
+        }
+        catch (err) {
+
+            reject(err)
+        }
+    })
+}
+
+function graficoAssociadosComparativo(response, sequelize) {
+
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            let montarResposta = {
+                Dia: {
+                    total: [],
+                    situacao: '',
+                    valor: []
+                },
+                Semana: {
+                    total: [],
+                    situacao: '',
+                    valor: []
+                },
+                Mes: {
+                    total: [],
+                    situacao: '',
+                    valor: []
+                }
+
+            }
+            const data = calcularDatas()
+
+            let situacaoHoje;
+            let situacaoSemana;
+            let situacaoMes;
+
+            let valorHoje;
+            let valorSemana;
+            let valorMes;
+
+            let totalAssociados = await sequelize.query("SELECT COUNT(*) AS count FROM todos_associados ")
+            totalAssociados = totalAssociados[0][0].count
+
+            // COMPARAR HOJE
+
+            // seleciona * associados de hoje
+            let associadosHoje = await sequelize.query("SELECT COUNT(*) AS count FROM todos_associados WHERE data_associacao = ?", {
+                replacements: [data.hoje],
+                types: sequelize.QueryTypes.SELECT
+            })
+
+            associadosHoje = associadosHoje[0][0].count
+
+            // seleciona * associados de ontem
+            let associadosHojeComparativo = await sequelize.query("SELECT COUNT(*) AS count FROM todos_associados WHERE data_associacao = ?", {
+                replacements: [data.dataHojeComparativo],
+                types: sequelize.QueryTypes.SELECT
+            })
+
+            associadosHojeComparativo = associadosHojeComparativo[0][0].count
+
+            if (associadosHoje > associadosHojeComparativo) {
+                situacaoHoje = 'aumentou'
+                valorHoje = associadosHoje - associadosHojeComparativo
+            }
+            else if (associadosHoje == associadosHojeComparativo) {
+                situacaoHoje = 'manteve'
+                valorHoje = associadosHoje
+
+            }
+            else {
+                situacaoHoje = 'diminuiu'
+                valorHoje = (associadosHoje - associadosHojeComparativo) * -1
+
+            }
+
+            // COMPARAR SEMANA
+
+            // seleciona * associados da semana
+            let associadosSemana = await sequelize.query("SELECT COUNT(*) AS count FROM todos_associados WHERE data_associacao >= ?", {
+                replacements: [data.dataSemana],
+                types: sequelize.QueryTypes.SELECT
+            })
+
+            associadosSemana = associadosSemana[0][0].count
+
+            // seleciona * associados de ontem
+            let associadosSemanaComparativo = await sequelize.query("SELECT COUNT(*) AS count FROM todos_associados WHERE data_associacao >= ? and data_associacao <= ?", {
+                replacements: [data.dataHojeComparativo, data.dataSemana],
+                types: sequelize.QueryTypes.SELECT
+            })
+
+            associadosSemanaComparativo = associadosSemanaComparativo[0][0].count
+
+            if (associadosSemana > associadosSemanaComparativo) {
+                situacaoSemana = 'aumentou'
+                valorSemana = associadosSemana - associadosSemanaComparativo
+            }
+            else if (associadosSemana == associadosSemanaComparativo) {
+                situacaoSemana = 'manteve'
+                valorSemana = associadosSemana
+
+            }
+            else {
+                situacaoSemana = 'diminuiu'
+                valorSemana = (associadosSemana - associadosSemanaComparativo) * -1
+
+            }
+
+            // COMPARAR MES
+
+            let associadosMes = await sequelize.query("SELECT COUNT(*) AS count FROM todos_associados WHERE data_associacao >= ?", {
+                replacements: [data.dataMes],
+                types: sequelize.QueryTypes.SELECT
+            })
+
+            associadosMes = associadosMes[0][0].count
+
+            // seleciona * associados de ontem
+            let associadosMesComparativo = await sequelize.query("SELECT COUNT(*) AS count FROM todos_associados WHERE data_associacao >= ? and data_associacao <= ?", {
+                replacements: [data.dataMesComparativo, data.dataMes],
+                types: sequelize.QueryTypes.SELECT
+            })
+
+            associadosMesComparativo = associadosMesComparativo[0][0].count
+
+            if (associadosMes > associadosMesComparativo) {
+                situacaoMes = 'aumentou'
+                valorMes = associadosMes - associadosMesComparativo
+            }
+            else if (associadosMes == associadosMesComparativo) {
+                situacaoMes = 'manteve'
+                valorMes = associadosMes 
+
+            }
+            else {
+                situacaoMes = 'diminuiu'
+                valorMes = (associadosMes - associadosMesComparativo) * -1
+
+            }
+
+
+            // MONTAR RESPOSTA
+            montarResposta.Dia.total = totalAssociados
+            montarResposta.Dia.situacao = situacaoHoje
+            montarResposta.Dia.valor = valorHoje
+
+            montarResposta.Semana.total = totalAssociados
+            montarResposta.Semana.situacao = situacaoSemana
+            montarResposta.Semana.valor = valorSemana
+
+            montarResposta.Mes.total = totalAssociados
+            montarResposta.Mes.situacao = situacaoMes
+            montarResposta.Mes.valor = valorMes
+            
+            response['Associado'] = montarResposta
+            resolve(response)
+
+        }
+        catch (err) {
+            reject(err)
+        }
+    })
+
+
+
+
+
+}
+
+function calcularDatas() {
+
+    let hoje = new Date();
+    let dataHojeComparativo;
+    let dataSemana;
+    let dataSemanaComparativo;
+    let dataMes;
+    let dataMesComparativo
+
+    dataHojeComparativo = new Date(hoje)
+    dataHojeComparativo.setDate(dataHojeComparativo.getDate() - 1)
+
+    dataSemana = new Date(hoje)
+    dataSemana.setDate(dataSemana.getDate() - 7)
+
+    dataSemanaComparativo = new Date(hoje)
+    dataSemanaComparativo.setDate(dataSemanaComparativo.getDate() - 14)
+
+    dataMes = new Date(hoje)
+    dataMes.setDate(dataMes.getDate() - 30)
+
+    dataMesComparativo = new Date(hoje)
+    dataMesComparativo.setDate(dataMesComparativo.getDate() - 60)
+
+    hoje.setHours(0, 0, 0, 0)
+    dataHojeComparativo.setHours(0, 0, 0, 0)
+    dataSemana.setHours(0, 0, 0, 0)
+    dataSemanaComparativo.setHours(0, 0, 0, 0)
+    dataMes.setHours(0, 0, 0, 0)
+    dataMesComparativo.setHours(0, 0, 0, 0)
+
+    return {
+        hoje,
+        dataHojeComparativo,
+        dataSemana,
+        dataSemanaComparativo,
+        dataMes,
+        dataMesComparativo
+    }
+}
+
+module.exports = { gerarGraficos, gerarGraficosComparativos }
--- src/routes/graficosRota.js
+++ src/routes/graficosRota.js
@@ -1,10 +1,10 @@
 const router = require("express").Router()
-const { gerarGraficos } = require("../controllers/graficosController")
+const { gerarGraficos, gerarGraficosComparativos } = require("../controllers/graficosController")
 const { authMiddleware } = require("../middleware/auth_funcionario")
 const { tratarMensagensDeErro } = require("../utils/errorMsg")
 
 
-router.get("/resultado", authMiddleware, async(req, res) => {
+router.get("/resultado", authMiddleware, async (req, res) => {
 
     try {
         const response = await gerarGraficos(req.sequelize)
@@ -14,10 +14,19 @@ router.get("/resultado", authMiddleware, async(req, res) => {
         const erroTratado = await tratarMensagensDeErro(err)
         res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
     }
-}
+})
 
+router.get("/comparativo", authMiddleware, async (req, res) => {
 
-)
+    try {
+        const response = await gerarGraficosComparativos(req.sequelize)
+        res.json(response)
+    }
+    catch (err) {
+        const erroTratado = await tratarMensagensDeErro(err)
+        res.status(erroTratado.status).json({ errMsg: erroTratado.message, "statusCode": erroTratado.status })
+    }
+})
 
 
 module.exports = router
